0503e724e745401a1852a041e021994907781d57
Javier Usobiaga <javier@swwweet.com>
2017-01-31 12:23:20 +0100

    Initial commit, empty template

---
+++ source/javascripts/body.js
@@ -0,0 +1,12 @@
+//= require jquery/dist/jquery
+//= require what-input/dist/what-input.min
+
+//= require foundation_requires
+
+//= require svg4everybody.min.js
+
+$(document).foundation();
+
+$(function(){
+  svg4everybody();
+});
